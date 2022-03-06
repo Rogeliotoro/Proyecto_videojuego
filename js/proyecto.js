1bc52500c9e574cbcf3abf3c5a33458a9027220b
Rogeliotoro <rogeltoro@gmail.com>
2022-03-06 13:25:19 +0100

    Funciones para el boton start y creacion de variables-prueba1

---
+++ js/proyecto.js
@@ -0,0 +1,17 @@
+let superior = document.getElementById("superior");
+let estadisticas = document.getElementsByClassName("estadisticas");
+let aangHp = document.getElementById("aangHp");
+let ZukoHp = document.getElementById("ZukoHp");
+let aangEstadisticas = document.getElementById ("aangEstadisticas");
+
+
+
+function startBattle(){
+    superior.innerHTML = "Empieza la batalla para saber el elemento mas fuerte";
+    estadisticas[0].style.visibility = "visible";
+
+}
+
+function aireControl (){
+
+}
