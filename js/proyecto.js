b208ae287dafa7b3019cdd6c00d1283842d2839a
Rogeliotoro <rogeltoro@gmail.com>
2022-03-06 20:24:49 +0100

    Finalizado boton de inicio, funcional con javascript

--- js/proyecto.js
+++ js/proyecto.js
@@ -1,17 +1,32 @@
-let superior = document.getElementById("superior");
-let estadisticas = document.getElementsByClassName("estadisticas");
-let aangHp = document.getElementById("aangHp");
-let ZukoHp = document.getElementById("ZukoHp");
-let aangEstadisticas = document.getElementById ("aangEstadisticas");
+let botonPrincipal = document.getElementById('botonPrincipal');
+let estadisticas = document.getElementsByClassName('estadisticas');
+var aangHp = document.getElementById('aangHp');
+let zukoHp = document.getElementById('ZukoHp');
+let aangEstadisticas = document.getElementById ('aangEstadisticas');
+let zukoEstadisticas = document.getElementById ('zukoEstadisticas');
+
+var aangHp =100;
+var zukohp =100;
 
 
-
-function startBattle(){
-    superior.innerHTML = "Empieza la batalla para saber el elemento mas fuerte";
-    estadisticas[0].style.visibility = "visible";
-
+function startBattle() {
+    botonPrincipal.innerHTML = "Quien sera el mas fuerte y listo!!";
+    for ( let i = 0; i<estadisticas.length; i++ ){
+        estadisticas[i].style.visibility= "visible";
+    }
 }
 
 function aireControl (){
+    let hitChance = Math.round(Math.random()*10);
+    if(hitChance <= 7){
+        let dmg= math.round(math.random()*10)+10;
+        zukoHp -= dmg;
+        botonPrincipal.innerHTML = "Ataque de Daño" + dmg + "el daño ahora es" + zukoHp+ "hp."
+        
+    }
+    else{
+        botonPrincipal.innerHTML = "has perdido"
+
+    }
 
 }
