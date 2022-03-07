9899f38d65091058a404685237a8628a48a9cfbf
Rogeliotoro <rogeltoro@gmail.com>
2022-03-07 20:18:31 +0100

    restruturacion del html, agregar 2 mas seleccion de jugadores e incorparacion de nuevos gif

--- js/proyecto.js
+++ js/proyecto.js
@@ -1,45 +1,21 @@
-let botonPrincipal = document.getElementById('botonPrincipal');
-let estadisticas = document.getElementsByClassName('estadisticas');
-var aangHp = document.getElementById('aangHp');
-let zukoHp = document.getElementById('ZukoHp');
-let aangEstadisticas = document.getElementById ('aangEstadisticas');
-let zukoEstadisticas = document.getElementById ('zukoEstadisticas');
+const botonPrincipal = document.getElementById('botonPrincipal');
+const estadisticas = document.getElementsByClassName('estadisticas');
+const aangHp = document.getElementById('aangHp');
+const zukoHp = document.getElementById('ZukoHp');
+const aangEstadisticas = document.getElementById ('aangEstadisticas');
+const zukoEstadisticas = document.getElementById ('zukoEstadisticas');
+const buttonStyle = document.getElementsByClassName('buttonStyle');
 
-var aangHp =1000;
-var zukohp =1000;
+const life1 = 100
+const life2 = 100
 
+///------------------------------------
 
 function startBattle() {
     botonPrincipal.innerHTML = "Quien sera el mas fuerte y listo!!";
-    for ( let i = 0; i<estadisticas.length; i++ ){
+    for ( let i = 0; i <= estadisticas.length; i++ ){
         estadisticas[i].style.visibility= "visible";
     }
 }
 
-function aireControl (){
-    let hitChance = Math.round(Math.random()*10);
-    if(hitChance <= 7){
-        let dmg= math.round(math.random()*10)+10;
-        zukoHp -= dmg;
-        botonPrincipal.innerHTML = "Ataque de Daño" + dmg + "el daño ahora es" + zukoHp+ "hp."
-        zukoHp.style.width =zukoHp + "px";
-    }
-    else{
-        botonPrincipal.innerHTML = "has perdido"
-
-    }
-
-}
-function fuegoControl (){
-    let hitChance = Math.round(Math.random()*10);
-    if(hitChance <= 7){
-        let dmg= math.round(math.random()*10)+10;
-        aangHp == dmg;
-        botonPrincipal.innerHTML = "Ataque de Daño" + dmg + "el daño ahora es" + aangHp+ "hp."
-    }
-    else{
-        botonPrincipal.innerHTML = "has perdido"
-
-    }
-
-}
+life1=life1-10
