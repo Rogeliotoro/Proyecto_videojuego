cb5b4daeeaa2b45004716da51233cfc2f54df08b
Rogeliotoro <rogeltoro@gmail.com>
2022-03-07 22:10:28 +0100

    Finalizacion de dar valores de clases creada en html

--- js/proyecto.js
+++ js/proyecto.js
@@ -5,17 +5,3 @@ const zukoHp = document.getElementById('ZukoHp');
 const aangEstadisticas = document.getElementById ('aangEstadisticas');
 const zukoEstadisticas = document.getElementById ('zukoEstadisticas');
 const buttonStyle = document.getElementsByClassName('buttonStyle');
-
-const life1 = 100
-const life2 = 100
-
-///------------------------------------
-
-function startBattle() {
-    botonPrincipal.innerHTML = "Quien sera el mas fuerte y listo!!";
-    for ( let i = 0; i <= estadisticas.length; i++ ){
-        estadisticas[i].style.visibility= "visible";
-    }
-}
-
-life1=life1-10
